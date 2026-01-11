0d98e32374d94430d252bdb7ccc4d2e75c510919
isaacIFSC <isaac.c17@aluno.ifsc.edu.br>
2026-01-11 18:28:26 -0300

    Base feita, Docker, DB e Spring inicializados

---
+++ src/test/java/tech/isaacmota/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.isaacmota.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
